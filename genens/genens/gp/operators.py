9ed8b9f09506a7fff17a3e620dc46c411ba92f09
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2019-03-03 23:03:06 +0100

    Small config change.

--- genens/genens/gp/operators.py
+++ genens/genens/gp/operators.py
@@ -7,23 +7,17 @@ import random
 # TODO need a primitive set, templates to create GpPrims
 
 
-def gen_half(pop_size, func_dict, term_dict, max_height, max_arity):
+def gen_half(pop_size, full_dict, grow_dict, term_dict, kwargs_dict, max_height, max_arity):
+    # grow
     for i in range(0, pop_size / 2):
-        yield gen_grow(func_dict, term_dict, max_height, max_arity)
-    
+        yield gen_tree({**full_dict, **grow_dict}, term_dict, max_height, max_arity, kwargs_dict)
+
+    # full
     for i in range(pop_size / 2, pop_size):
-        yield gen_full(func_dict, term_dict, max_height, max_arity)
+        yield gen_tree(full_dict, term_dict, max_height, max_arity, kwargs_dict)
 
 
-def gen_grow(func_dict, term_dict, max_height, max_arity):
-    return gen_tree(func_dict + term_dict, term_dict, max_height, max_arity)
-
-
-def gen_full(func_dict, term_dict, max_height, max_arity):
-    return gen_tree(func_dict, term_dict, max_height, max_arity)
-
-
-def gen_tree(full_dict, term_dict, max_height, max_arity):
+def gen_tree(full_dict, term_dict, max_height, max_arity, kwargs_dict):
     tree_list = []
     type_stack = [('out', 1, 1)]
     
@@ -37,14 +31,11 @@ def gen_tree(full_dict, term_dict, max_height, max_arity):
             choose_from = full_dict[next_type]
         else:
             choose_from = term_dict[next_type]
-            term = random.choice(choose_from)
-            tree_list.append(term)
-            continue
         
         # template of the next primitive
         next_prim_t = random.choice(choose_from)
         
-        prim = next_prim_t.create_primitive(max_arity)
+        prim = next_prim_t.create_primitive(max_arity, kwargs_dict)
         
         if prim.arity > 0:
             for child_type in prim.node_type[0]:
