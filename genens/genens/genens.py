825e45472a913cf52991eb5ccbf76e0c959e30e0
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2019-03-30 22:28:13 +0100

    Added height and arity parameters to Classifier and Regressor.

--- genens/genens/genens.py
+++ genens/genens/genens.py
@@ -9,19 +9,21 @@ from sklearn.base import ClassifierMixin, RegressorMixin
 
 class GenensClassifier(GenensBase, ClassifierMixin):
     def __init__(self, config=None, n_jobs=1, cx_pb=0.5, mut_pb=0.1, mut_args_pb=0.3, scorer=None,
-                 pop_size=100, n_gen=10, hc_repeat=0, hc_keep_last=False):
+                 pop_size=100, n_gen=10, hc_repeat=0, hc_keep_last=False, max_height=None,
+                 max_arity=None):
         if config is None:
             config = create_clf_config()
 
         super().__init__(config, n_jobs, cx_pb, mut_pb, mut_args_pb, scorer, pop_size,
-                         n_gen, hc_repeat, hc_keep_last)
+                         n_gen, hc_repeat, hc_keep_last, max_height, max_arity)
 
 
 class GenensRegressor(GenensBase, RegressorMixin):
     def __init__(self, config=None, n_jobs=1, cx_pb=0.5, mut_pb=0.1, mut_args_pb=0.3, scorer=None,
-                 pop_size=100, n_gen=10, hc_repeat=0, hc_keep_last=False):
+                 pop_size=100, n_gen=10, hc_repeat=0, hc_keep_last=False, max_height=None,
+                 max_arity=None):
         if config is None:
             config = create_regr_config()
 
         super().__init__(config, n_jobs, cx_pb, mut_pb, mut_args_pb, scorer, pop_size,
-                         n_gen, hc_repeat, hc_keep_last)
+                         n_gen, hc_repeat, hc_keep_last, max_height, max_arity)
