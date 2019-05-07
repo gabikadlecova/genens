015c241fa14c3572d1634e5dd63d07d79260df44
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2019-05-07 10:34:16 +0200

    Move print

--- genens/tests/process_results.py
+++ genens/tests/process_results.py
@@ -46,14 +46,14 @@ def get_openml_stats():
     accuracy_df = pd.DataFrame(columns=['task_id', 'name', 'accuracy'])
 
     for task_id in benchmark_suite.tasks:
+        print('Task {}'.format(task_id))
+
         evaluations = openml.evaluations.list_evaluations('predictive_accuracy', task=[task_id])
 
         vals = [(e.task_id, e.data_name, e.value) for e in evaluations.values()]
         df = pd.DataFrame(vals, columns=['task_id', 'name', 'accuracy'])
 
         accuracy_df = accuracy_df.append(df)
-
-        print('Task {}'.format(task_id))
 
     return accuracy_df
 
